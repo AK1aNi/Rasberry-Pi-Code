c2c60c90c508cd032cedc70750e930d0029e0b8c
Muhammad Abdullah Kiani <22MKiani@heckgrammar.co.uk>
2026-05-01 12:17:19 +0100

    idek

--- Program.cs
+++ Program.cs
@@ -1,14 +1,49 @@
 ﻿using System.Device.Gpio;
+using System.Threading;
 namespace Rasberry_Pi_Code
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            GpioController controller = new GpioController(PinNumberingScheme.Board);
-            controller.OpenPin(13, PinMode.Output);
-            controller.Write(13, PinValue.High);
 
+            GpioController controller = new GpioController(PinNumberingScheme.Board);
+            int pin = 16;
+            int lightTime = 500;
+
+            controller.OpenPin(pin, PinMode.Output);
+            Console.Write("On ");
+            for (int i =0; i < 10; i++)
+            {
+                controller.Write(pin, PinValue.High);
+
+                Thread.Sleep(lightTime);
+                Console.Write("Off ");
+                controller.Write(pin, PinValue.Low);
+
+                Thread.Sleep(lightTime);
+            }
         }
     }
 }
+using System.Device.Gpio;
+using System.Threading;
+
+
+GpioController controller = new GpioController(PinNumberingScheme.Board);
+int pin = 16;
+int lightTime = 10000;
+
+controller.OpenPin(pin, PinMode.Output);
+
+
+Console.Write("On ");
+controller.Write(pin, PinValue.High);
+
+Thread.Sleep(lightTime);
+Console.Write("Off ");
+controller.Write(pin, PinValue.Low);
+
+Thread.Sleep(lightTime);
+
+controller.ClosePin(pin);
